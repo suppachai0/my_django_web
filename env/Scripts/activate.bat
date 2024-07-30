71ecd4b37e276a928c7afa3aed829b73cdece246
Suppachai Wicheer <suppachai4454@gmail.com>
2024-07-30 13:08:06 +0700

    commit

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Lenovo\Documents\my_wed\env
+set VIRTUAL_ENV=D:\forstudy_python\my_web\env
 
 if not defined PROMPT set PROMPT=$P$G
 
